c9236cb4fa84873c36d53863adb3e807a70a655e
Loc <buidinhloc231@gmail.com>
2025-02-24 21:20:43 +0700

    code cũ sai vl

--- 2014/11/Gen.cpp
+++ 2014/11/Gen.cpp
@@ -1,90 +1,63 @@
-int poww(int a, int b, int mod) {
-    if (b == 0) return 1;
-    int tmp = poww(a, b / 2, mod);
-    if (b & 1) return ((tmp * tmp) % mod * a) % mod;
-    return (tmp * tmp) % mod; 
-}
-
-bool checkComposite(int n, int a, int d, int s) {
-    int x = poww(a, d, n);
-    if (x == 1 or x == n - 1) return 0;
-
-    FOR(r, 1, s - 1) {
-        x = x * x % n;
-        if (x == n - 1) return 0;
-    }
-    return 1;
-}
-
-bool checkPrime(int n) {
-    if (n < 2) return 0;
-    if (n % 2 == 0) return (n == 2);
-
-    int d = n - 1;
-    int s = 0;
-
-    while(d % 2 == 0) {
-        d /= 2;
-        s++;
-    }
-
-    vector<int> base = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
-
-    for(const int &val : base) {
-        if (val > n - 2) break;
-        if (checkComposite(n, val, d, s)) return 0;
-    }
-    return 1;
-}
-
-bool check(int x) {
-    if (x == 1) return 1;
-    int xx = sqrt(x);
-    if (xx * xx == x) 
-        if (checkPrime(xx)) return 1;
-    xx = cbrt(x);
-    if (xx * xx * xx == x) 
-        if (checkPrime(xx)) return 1;
-    return 0;
-}
-
-vector<int> split(const string &s) {
-    vector<int> ret;
-    int num = 0;
-    FOR(i, 0, len(s) - 1) {
-        if (s[i] == ' ') {
-            if (check(num)) ret.pb(num);
-            num = 0;
-        } else num = num * 10 + (s[i] - '0');
-    }
-    ret.pb(num);
-    return ret;
-}
-
-int n, m;
-vector<int> a, b;
-
 const int maxn = 1007;
-int f[maxn][maxn];
-int dp(int i, int j) {
-    if (i > n or j > m) return 0;
-    int &ans = f[i][j];
-    if (ans != -1) return ans;
+int n, m, f[maxn][maxn];
+ull a[maxn], b[maxn];
 
-    ans = 0;
-    if (a[i] == b[j]) maxi(ans, dp(i + 1, j + 1) + 1);
-    maxi(ans, dp(i + 1, j));
-    maxi(ans, dp(i, j + 1));
+ull cbrtt(ull x) {
+    ull l = 1, r = 2154434, ans;
+    while(l <= r) {
+        ull mid = (l + r) >> 1;
+        if (mid * mid * mid <= x) {
+            ans = mid;
+            l = mid + 1;
+        } else r = mid - 1;
+    }
     return ans;
 }
 
-void process() {
-    string s; 
-    getline(cin, s);
-    a = split(s); n = len(a) - 1;
-    getline(cin, s);
-    b = split(s); m = len(b) - 1;
+bool checkPrime(ull x) {
+    if (x < 2) return 0;
 
-    memo(f, -1);
-    cout << dp(0, 0);
+    for(int i = 2; i <= 5 and i * i <= x; i++) 
+        if (x % i == 0) return 0;
+    for(ull k = 1; (6 * k + 1) * (6 * k + 1) <= x; k++)
+        if (x % (6 * k + 1) == 0 or x % (6 * k + 5) == 0) return 0;
+    return 1;
+}
+
+bool check(ull x) {
+    if (x == 0) return 0;
+    ull xx = sqrt(x);
+    if (xx * xx == x) return 1;
+
+    xx = cbrtt(x);
+    return (xx * xx * xx == x and checkPrime(xx));
+}
+
+void split(ull *a, int &n, string s) {
+    s = s + ' ';
+    ull num = 0;
+
+    FOR(i, 0, len(s) - 1) {
+        if (s[i] == ' ') {
+            if (check(num)) a[++n] = num;
+            num = 0;
+        } else num = num * 10 + (s[i] - '0');
+    }
+}
+
+void process() {
+    string s;
+
+    getline(cin, s);
+    split(a, n, s);
+    getline(cin, s);
+    split(b, m, s);
+
+    FOR(i, 1, n) FOR(j, 1, m) {
+        f[i][j] = max(f[i - 1][j], f[i][j - 1]);
+        if (a[i] == b[j])
+            maxi(f[i][j], f[i - 1][j - 1] + 1);
+    }
+
+    cout << f[n][m];
 }
